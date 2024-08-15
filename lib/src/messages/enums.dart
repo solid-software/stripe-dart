fa41c00bff425f08a692610dc718804b20fbb887
solid-maksymtielnyi <maksym.tielnyi@solid.software>
2024-08-15 07:20:17 +0300

    Export invoice_expandable_field.dart

--- lib/src/messages/enums.dart
+++ lib/src/messages/enums.dart
@@ -1,3 +1,4 @@
+export 'enums/expandable_fields/invoice_expandable_field.dart';
 export 'enums/expandable_fields/subscription_expandable_field.dart';
 export 'enums/payment_behavior.dart';
 export 'enums/proration_behavior.dart';
