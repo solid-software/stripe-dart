697111cee9a618fe4309e361283ba7ebc3b5df7a
solid-maksymtielnyi <maksym.tielnyi@solid.software>
2025-07-14 19:46:16 +0300

    Add doc comments to customer_balance_transaction request models

--- lib/src/messages/requests/create_customer_balance_transaction_request.dart
+++ lib/src/messages/requests/create_customer_balance_transaction_request.dart
@@ -1,13 +1,26 @@
 part of '../../../messages.dart';
 
+/// https://docs.stripe.com/api/customer_balance_transactions/create
 @JsonSerializable()
 class CreateCustomerBalanceTransactionRequest {
+  /// The integer amount in cents to apply to the customer’s credit balance.
   final int amount;
 
+
+  /// Three-letter ISO currency code, in lowercase. Must be a supported
+  /// currency. Specifies the invoice_credit_balance that this transaction will
+  /// apply to. If the customer’s currency is not set, it will be updated to
+  /// this value.
   final String currency;
 
+  /// An arbitrary string attached to the object. Often useful for displaying to
+  /// users.
   final String? description;
 
+  /// Set of key-value pairs that you can attach to an object. This can be
+  /// useful for storing additional information about the object in a structured
+  /// format. Individual keys can be unset by posting an empty value to them.
+  /// All keys can be unset by posting an empty value to metadata.
   final Map<String, dynamic>? metadata;
 
   CreateCustomerBalanceTransactionRequest({
--- lib/src/messages/requests/update_customer_balance_transaction_request.dart
+++ lib/src/messages/requests/update_customer_balance_transaction_request.dart
@@ -1,9 +1,16 @@
 part of '../../../messages.dart';
 
+/// https://docs.stripe.com/api/customer_balance_transactions/update
 @JsonSerializable()
 class UpdateCustomerBalanceTransactionRequest {
+  /// An arbitrary string attached to the object. Often useful for displaying to
+  /// users.
   final String? description;
 
+  /// Set of key-value pairs that you can attach to an object. This can be
+  /// useful for storing additional information about the object in a structured
+  /// format. Individual keys can be unset by posting an empty value to them.
+  /// All keys can be unset by posting an empty value to metadata.
   final Map<String, dynamic>? metadata;
 
   UpdateCustomerBalanceTransactionRequest({
