8b3db0727ffa0694c3f718bf2168ede14800d525
solid-maksymtielnyi <maksym.tielnyi@solid.software>
2025-09-04 03:21:25 +0300

    Fix file contents endpoints paths

--- lib/src/resources/file.dart
+++ lib/src/resources/file.dart
@@ -11,13 +11,21 @@ class FileResource extends Resource<File> {
   FileResource(Client client) : super(client);
 
   Future<String> downloadContentPlain(String fileId) async {
-    final content = await getPlain('$_resourceName/$fileId');
+    final content = await getPlain(_buildContentsPath(fileId));
 
     return content;
   }
 
+  String _buildContentsPath(String fileId) {
+    return [
+      _resourceName,
+      fileId,
+      'contents',
+    ].join('/');
+  }
+
   Future<Uint8List> downloadContentBytes(String fileId) async {
-    final bytes = await getBytes('$_resourceName/$fileId');
+    final bytes = await getBytes(_buildContentsPath(fileId));
 
     return bytes;
   }
