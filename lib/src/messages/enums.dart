0451057c5e609adfb88d134b57011fc832a6f675
solid-maksymtielnyi <maksym.tielnyi@solid.software>
2025-10-08 03:46:17 +0300

    Export checkout_session_expandable_field.dart

--- lib/src/messages/enums.dart
+++ lib/src/messages/enums.dart
@@ -1,5 +1,6 @@
 export 'enums/expandable_fields/invoice_expandable_field.dart';
 export 'enums/expandable_fields/subscription_expandable_field.dart';
+export 'enums/expandable_fields/checkout_session_expandable_field.dart';
 export 'enums/payment_behavior.dart';
 export 'enums/proration_behavior.dart';
 export 'enums/stripe_api_error_type.dart';
